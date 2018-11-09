e55911b45045ca3850083898edc2b260a37ed416
ChriZ982 <christopher.zinda2@googlemail.com>
2018-11-09 20:22:24 +0100

    Used correct project name

--- build.bat
+++ build.bat
@@ -4,14 +4,14 @@ set build_tools="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\
 
 set out_dir=build\debug
 set compilerflags=/GL /Gy /Zi /Gm- /sdl /Fd"%out_dir%\vc141.pdb" /Zc:inline /MD /EHsc /nologo /Fo"%out_dir%/"
-set linkerflags=/PDB:"%out_dir%/InstallMonitor.pdb" /DEBUG /OPT:REF /OPT:ICF
+set linkerflags=/PDB:"%out_dir%/OH2RulesGenerator.pdb" /DEBUG /OPT:REF /OPT:ICF
 md %out_dir%
 
 call %build_tools% x64
-cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\OH2RulesGenerator-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
 
 call %build_tools% x86
-cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\OH2RulesGenerator-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
 
 set out_dir=build\release
 set compilerflags=/GL /Gy /Gm- /Ox /sdl /Zc:inline /Oi /MD /EHsc /nologo /Fo"%out_dir%/"
@@ -19,7 +19,7 @@ set linkerflags=/OPT:REF /OPT:ICF
 md %out_dir%
 
 call %build_tools% x64
-cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\OH2RulesGenerator-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
 
 call %build_tools% x86
-cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\OH2RulesGenerator-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
